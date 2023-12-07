2a6c0d8e39bf9687b1a29e73a4544af3a5c53d4c
aiaiaiai1 <cvc100326@naver.com>
2023-12-07 17:36:15 +0900

    fix: dto 매핑관련 오류 수정

--- src/main/java/rmap/request/BuildNotionRequest.java
+++ src/main/java/rmap/request/BuildNotionRequest.java
@@ -1,17 +1,20 @@
 package rmap.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor
 public class BuildNotionRequest {
 
     private String name;
     private String content;
     private RelatedNotionInfo relatedNotion;
+
+    public BuildNotionRequest() {
+    }
 
 }
 
