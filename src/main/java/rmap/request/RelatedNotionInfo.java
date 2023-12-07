79ca1355bf46dfda4856ddcafbc4c37299692781
aiaiaiai1 <cvc100326@naver.com>
2023-12-07 22:04:28 +0900

    fix: dto 매핑관련 오류 수정 완료
    - @RequestBody와 관련해서 빈생성자 추가

--- src/main/java/rmap/request/RelatedNotionInfo.java
+++ src/main/java/rmap/request/RelatedNotionInfo.java
@@ -1,12 +1,16 @@
 package rmap.request;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class RelatedNotionInfo {
 
-    private final Long id;
+    private Long id;
 
+    public RelatedNotionInfo(Long id) {
+        this.id = id;
+    }
+
+    public RelatedNotionInfo() {
+    }
 }
