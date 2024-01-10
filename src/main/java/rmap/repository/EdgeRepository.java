f4f99d9884f2bdfcb804a38fd6efdd675a5aee3b
aiaiaiai1 <cvc100326@naver.com>
2024-01-10 21:13:02 +0900

    fix: 4차 스프린트 버그 수정

--- src/main/java/rmap/repository/EdgeRepository.java
+++ src/main/java/rmap/repository/EdgeRepository.java
@@ -7,10 +7,11 @@ import rmap.entity.Edge;
 
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
 
-    @Query(value = "select * from edge as e "
+    @Query(value = "select e.id, e.source_notion_id, e.target_notion_id, e.description "
+            + "from edge as e "
             + "join notion as n on n.id = e.source_notion_id "
             + "or n.id = e.target_notion_id "
-            + "where n.id = :notionId",nativeQuery = true)
+            + "where n.id = :notionId", nativeQuery = true)
     List<Edge> findAllByNotionId(Long notionId);
 
 //    @Query(value = "delete from edge where id in :edgeIds", nativeQuery = true)
