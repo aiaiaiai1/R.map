2f2a26500630c525ed6932978c1fe332aa4453b8
aiaiaiai1 <cvc100326@naver.com>
2024-03-11 16:41:17 +0900

    fix: jpql문 수정

--- src/main/java/rmap/repository/EdgeRepository.java
+++ src/main/java/rmap/repository/EdgeRepository.java
@@ -7,12 +7,14 @@ import rmap.entity.Edge;
 
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
 
-    //    @Query(value = "select e.id, e.source_notion_id, e.target_notion_id, e.description "
+//        @Query(value = "select e.id, e.source_notion_id, e.target_notion_id, e.description "
 //            + "from edge as e "
 //            + "join notion as n on n.id = e.source_notion_id "
 //            + "or n.id = e.target_notion_id "
 //            + "where n.id = :notionId", nativeQuery = true)
-    @Query("select e from Notion n join n.edges.edges e where n.id = :notionId")
+    @Query("select e from Notion n "
+            + "join Edge e on e.sourceNotion = n or e.targetNotion = n "
+            + "where n.id = :notionId")
     List<Edge> findAllByNotionId(Long notionId);
 
 }
