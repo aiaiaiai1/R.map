8aa764635ceed07cce182d3bde1e00c4ce3bb961
aiaiaiai1 <cvc100326@naver.com>
2024-03-17 18:34:27 +0900

    feat: 노션폴더 내에 그래프가 한개이면 분리 안되도록 로직 추가

--- src/main/java/rmap/exception/type/NotionFolderExceptionType.java
+++ src/main/java/rmap/exception/type/NotionFolderExceptionType.java
@@ -8,7 +8,13 @@ public enum NotionFolderExceptionType implements ExceptionType {
             400,
             "노션 폴더가 존재하지 않습니다.",
             "노션폴더가 존재하지 않는 경우"
-    );
+    ),
+    CAN_NOT_SPLIT(
+            401,
+                    "노션 폴더를 분리 할 수 없습니다.",
+                    "노션폴더내애 그래프가 하나인 경우"
+    ),
+    ;
 
     private final int errorCode;
     private final String message;
