692b0b67f339f47e278b18f4db4926b978f7047a
aiaiaiai1 <cvc100326@naver.com>
2024-04-02 14:05:33 +0900

    fix: http 메서드 수정

--- src/main/java/rmap/controller/NotionFolderController.java
+++ src/main/java/rmap/controller/NotionFolderController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -83,7 +84,7 @@ public class NotionFolderController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Void> editNotionFolderName(
             @PathVariable("id") Long notionFolderId,
             @RequestBody EditNotionFolderNameRequest request
