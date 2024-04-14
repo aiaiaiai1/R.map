9ba480b6ad388ab50c8a975d3483457d668273fc
aiaiaiai1 <cvc100326@naver.com>
2024-04-14 10:51:02 +0900

    fix: @RequestBody 어노테이션 추가

--- src/main/java/rmap/controller/SignUpController.java
+++ src/main/java/rmap/controller/SignUpController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import rmap.entity.UserAccount;
@@ -22,25 +23,25 @@ public class SignUpController {
     private final SignUpService signUpService;
 
     @PostMapping("/user/email/auth-request")
-    public ResponseEntity<Void> sendAuthentication(@Valid SendAuthenticationRequest request) {
+    public ResponseEntity<Void> sendAuthentication(@Valid @RequestBody SendAuthenticationRequest request) {
         signUpService.sendAuthenticationTo(request.getEmail());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/user/email/auth-check")
-    public ResponseEntity<Void> verifyAuthentication(@Valid VerificationRequest request) {
+    public ResponseEntity<Void> verifyAuthentication(@Valid @RequestBody VerificationRequest request) {
         signUpService.verifyAuthentication(request.getEmail(), request.getCode());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/user/welcome")
-    public ResponseEntity<Void> signUp(@Valid VerificationRequest request) {
+    public ResponseEntity<Void> signUp(@Valid @RequestBody VerificationRequest request) {
         signUpService.signUp(request.getEmail(), request.getCode());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/user/hello")
-    public ResponseEntity<Void> logIn(@Valid LoginRequest loginRequest, HttpServletRequest request) {
+    public ResponseEntity<Void> logIn(@Valid @RequestBody LoginRequest loginRequest, HttpServletRequest request) {
         UserAccount userAccount = signUpService.logIn(loginRequest.getEmail(), loginRequest.getPassword());
         HttpSession session = request.getSession();
         session.setAttribute("logined", userAccount);
