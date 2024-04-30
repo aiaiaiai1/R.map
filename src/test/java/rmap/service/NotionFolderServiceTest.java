b72e9f13396b98831d623ebd022912a1eedc2bb2
aiaiaiai1 <cvc100326@naver.com>
2024-05-01 00:56:44 +0900

    test: 노션폴더 소유자가 아닌경우 예외 발생 테스트 코드 추가

--- src/test/java/rmap/service/NotionFolderServiceTest.java
+++ src/test/java/rmap/service/NotionFolderServiceTest.java
@@ -1,5 +1,6 @@
 package rmap.service;
 
+import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,6 +10,9 @@ import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.times;
 import static rmap.EntityCreationSupporter.노션_생성;
 import static rmap.EntityCreationSupporter.노션_폴더_생성;
+import static rmap.EntityCreationSupporter.유저_생성;
+import static rmap.Fixtures.TEST_EMAIL;
+import static rmap.Fixtures.TEST_PASSWORD;
 import static rmap.Fixtures.알맵이;
 import static rmap.Fixtures.알맵이의_노션_폴더_알파벳;
 import static rmap.Fixtures.알맵이의_노션_폴더_음식;
@@ -44,7 +48,7 @@ class NotionFolderServiceTest extends ServiceTest {
         Notion 노션_배 = 노션_생성(2L, "배", "", 알맵이의_노션_폴더_음식);
 
         given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_음식.getId())).willReturn(알맵이의_노션_폴더_음식);
-        given(notionRepository.findAllInNotionFolder(알맵이의_노션_폴더_음식.getId())).willReturn(List.of(노션_사과, 노션_배));
+        given(notionRepository.findAllInNotionFolder(알맵이의_노션_폴더_음식.getId())).willReturn(of(노션_사과, 노션_배));
 
         // when
         NotionFolderResponse response = notionFolderService.readNotionFolderInfo(알맵이의_노션_폴더_음식.getId());
@@ -55,32 +59,59 @@ class NotionFolderServiceTest extends ServiceTest {
         assertThat(results.get(1).getId()).isEqualTo(노션_사과.getId());
     }
 
-    @Test
-    void 기존의_노션_폴더_여러개를_새로운_노션_폴더_한개로_합친다() {
-        // given
-        Notion notion1 = 노션_생성(1L, "A", "a", 알맵이의_노션_폴더_알파벳);
-        Notion notion2 = 노션_생성(2L, "사과", "과일", 알맵이의_노션_폴더_음식);
 
-        NotionFolder newNotionFolder = 노션_폴더_생성(1L, 알맵이, "짬뽕");
+    @Nested
+    class 노션_합치기 {
+        @Test
+        void 기존의_노션_폴더_여러개를_새로운_노션_폴더_한개로_합친다() {
+            // given
+            Notion notion1 = 노션_생성(1L, "A", "a", 알맵이의_노션_폴더_알파벳);
+            Notion notion2 = 노션_생성(2L, "사과", "과일", 알맵이의_노션_폴더_음식);
 
-        given(notionFolderRepository.save(any(NotionFolder.class))).willReturn(newNotionFolder);
-        given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_알파벳.getId())).willReturn(알맵이의_노션_폴더_알파벳);
-        given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_음식.getId())).willReturn(알맵이의_노션_폴더_음식);
-        given(notionRepository.findAllInNotionFolder(알맵이의_노션_폴더_알파벳.getId())).willReturn(List.of(notion1));
-        given(notionRepository.findAllInNotionFolder(알맵이의_노션_폴더_음식.getId())).willReturn(List.of(notion2));
-        willDoNothing().given(notionFolderRepository).deleteById(any(Long.class));
+            NotionFolder newNotionFolder = 노션_폴더_생성(1L, 알맵이, "짬뽕");
 
-        // when
-        notionFolderService.mergeNotionFolderWithNew(
-                알맵이,
-                "new",
-                List.of(알맵이의_노션_폴더_음식.getId(), 알맵이의_노션_폴더_알파벳.getId())
-        );
+            given(notionFolderRepository.save(any(NotionFolder.class))).willReturn(newNotionFolder);
+            given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_알파벳.getId())).willReturn(알맵이의_노션_폴더_알파벳);
+            given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_음식.getId())).willReturn(알맵이의_노션_폴더_음식);
+            given(notionRepository.findAllInNotionFolder(알맵이의_노션_폴더_알파벳.getId())).willReturn(of(notion1));
+            given(notionRepository.findAllInNotionFolder(알맵이의_노션_폴더_음식.getId())).willReturn(of(notion2));
+            willDoNothing().given(notionFolderRepository).deleteById(any(Long.class));
 
-        // then
-        assertThat(notion1.getNotionFolder()).isEqualTo(newNotionFolder);
-        assertThat(notion2.getNotionFolder()).isEqualTo(newNotionFolder);
-        then(notionFolderRepository).should(times(2)).deleteById(any(Long.class));
+            // when
+            notionFolderService.mergeNotionFolderWithNew(
+                    알맵이,
+                    "new",
+                    of(알맵이의_노션_폴더_음식.getId(), 알맵이의_노션_폴더_알파벳.getId())
+            );
+
+            // then
+            assertThat(notion1.getNotionFolder()).isEqualTo(newNotionFolder);
+            assertThat(notion2.getNotionFolder()).isEqualTo(newNotionFolder);
+            then(notionFolderRepository).should(times(2)).deleteById(any(Long.class));
+        }
+
+        @Test
+        void 노션_폴더의_소유자가_아닌_경우_예외가_발생한다() {
+            Notion notion1 = 노션_생성(1L, "A", "a", 알맵이의_노션_폴더_알파벳);
+
+            User user = 유저_생성(1L, TEST_EMAIL, TEST_PASSWORD);
+            NotionFolder notionFolder = 노션_폴더_생성(1L, user, "짬뽕");
+            Notion notion2 = 노션_생성(2L, "사과", "과일", notionFolder);
+
+            NotionFolder newNotionFolder = 노션_폴더_생성(2L, 알맵이, "new");
+
+            given(notionFolderRepository.save(any(NotionFolder.class))).willReturn(newNotionFolder);
+            given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_알파벳.getId())).willReturn(알맵이의_노션_폴더_알파벳);
+            given(notionFolderRepository.findByIdOrThrow(notionFolder.getId())).willReturn(notionFolder);
+
+            // when, then
+            assertThatThrownBy(() -> notionFolderService.mergeNotionFolderWithNew(
+                    알맵이,
+                    "new",
+                    List.of(알맵이의_노션_폴더_알파벳.getId(), notionFolder.getId())))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("권한이 없습니다.");
+        }
     }
 
     @Nested
@@ -112,6 +143,30 @@ class NotionFolderServiceTest extends ServiceTest {
             assertThat(notionA.getNotionFolder()).isEqualTo(알맵이의_노션_폴더_알파벳);
             assertThat(notionB.getNotionFolder()).isEqualTo(newNotionFolder);
             assertThat(notionC.getNotionFolder()).isEqualTo(newNotionFolder);
+        }
+
+        @Test
+        void 노션_폴더의_소유자가_아닌_경우_예외가_발생한다() {
+            // given
+            Notion notionA = 노션_생성(1L, "A", "a", 알맵이의_노션_폴더_알파벳);
+            Notion notionB = 노션_생성(2L, "B", "과일", 알맵이의_노션_폴더_알파벳);
+            Notion notionC = 노션_생성(3L, "C", "과일", 알맵이의_노션_폴더_알파벳);
+
+            User user = 유저_생성(1L,TEST_EMAIL,TEST_PASSWORD);
+
+            NotionFolder newNotionFolder = 노션_폴더_생성(1L, user, "new");
+
+            notionB.connect(notionC, "");
+            notionC.connect(notionB, "");
+
+            given(notionFolderRepository.findByIdOrThrow(알맵이의_노션_폴더_알파벳.getId())).willReturn(알맵이의_노션_폴더_알파벳);
+
+            // when, then
+            assertThatThrownBy(() -> notionFolderService.splitNotionFolderWithNew(user, newNotionFolder.getName(),
+                    알맵이의_노션_폴더_알파벳.getId(), notionB.getId()))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("권한이 없습니다.");
+
         }
 
         @Test
