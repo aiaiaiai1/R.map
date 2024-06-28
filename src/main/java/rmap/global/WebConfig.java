8b6db0801126e16a6ee1064604f2ed0c13b04eef
aiaiaiai1 <cvc100326@naver.com>
2024-06-29 02:26:00 +0900

    fix: cors 설정

--- src/main/java/rmap/global/WebConfig.java
+++ src/main/java/rmap/global/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedOriginPatterns("*")
-                .allowedHeaders(HttpHeaders.COOKIE)
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 
